50a46246030752ed5837d3e20ab260a3ee9d49eb
ketsumi <mewtropolis.ketsumi@gmail.com>
2019-04-12 08:50:06 -0400

    new file structure

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 
-app.get('/', (req, res) => {
-	res.sendFile('./index.html');
+const staticPath = path.resolve(__dirname, './');
+const indexPath = path.resolve(__dirname, './index.html');
+
+app.use(express.json());
+app.use(express.static(staticPath));
+
+app.get('*', (req, res) => {
+	res.sendFile(indexPath);
 });
 
-app.listen(3000, () => console.log('connected on port 3000'));
+app.listen(4000, () => console.log('connected on port 4000'));
