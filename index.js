5c2f7e603f821e1510813a09731786ae23b9db9d
ketsumi <mewtropolis.ketsumi@gmail.com>
2019-04-14 07:37:12 -0400

    first finished and workable version

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
+'use strict';
+
 const express = require('express');
 const app = express();
 const path = require('path');
 
-const staticPath = path.resolve(__dirname, './');
-const indexPath = path.resolve(__dirname, './index.html');
+const staticPath = path.resolve(__dirname, './public/');
+const indexPath = path.resolve(__dirname, './public/index.html');
 
 app.use(express.json());
 app.use(express.static(staticPath));
