8d6fe814f5add1140e9a85af5c805b7576394f7f
ketsumi <mewtropolis.ketsumi@gmail.com>
2019-04-14 08:03:40 -0400

    renamed public folder to docs

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-const staticPath = path.resolve(__dirname, './public/');
-const indexPath = path.resolve(__dirname, './public/index.html');
+const staticPath = path.resolve(__dirname, './docs/');
+const indexPath = path.resolve(__dirname, './docs/index.html');
 
 app.use(express.json());
 app.use(express.static(staticPath));
